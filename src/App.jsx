5c24c793d6497c3aaa5b537661a61270c82f6a77
Jeancarlo <jcode2006@gmail.com>
2022-02-13 17:01:15 -0500

    🔥

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* **************** Imports **************** */
+import React from 'react'
+import logo from './logo.svg'
+import Header from './components/Header'
+
+/* **************** Variables **************** */
+
+/* **************** Functions **************** */
+function App() {
+  return (
+    <>
+      <Header logo={logo} title='React App' />
+    </>
+  )
+}
+
+/* **************** Code Execution **************** */
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+/* **************** Imports **************** */
+import React from 'react'
+
+/* **************** Variables **************** */
+
+/* **************** Functions **************** */
+const Header = ({ logo, title }) => <header className='App-header'></header>
+
+/* **************** Code Execution **************** */
+export default Header
